2ca42df596c7273e32834885fe8fc71dd8aec3ef
ghannam88 <mr.ghannam@gmail.com>
2021-04-17 02:48:43 +0300

    adding the story
    the the project

---
+++ story.js
@@ -0,0 +1,5 @@
+let agent = "g";
+let age = 17;
+let hobby = "cars";
+
+console.log(`Mr./Mrs. ${agent}, in 33 years, you will be ${age + 33} years old. By then you'll have decades of ${hobby} practice!`);
